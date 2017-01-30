04ed1f02a161771daa5722eb7fcc1b97d3b51f57
Jesse Weisner <jesse@weisner.ca>
2017-01-30 13:08:13 -0800

    not needed

--- EXAMPLE_Config.cpp
+++
@@ -1,6 +0,0 @@
-            class ClaimCrate: ExileAbstractAction
-            {
-                title = "Claim Ownership";
-                condition = "true";
-                action = "call ExileClient_ClaimCrates_network_claimRequestSend";
-            };
