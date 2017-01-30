b64f702a2b2bb8570fb52d50631ea677f55e4197
Jesse Weisner <jesse@weisner.ca>
2017-01-29 19:44:31 -0800

    whitespace

--- EXAMPLE_Config.cpp
+++ EXAMPLE_Config.cpp
@@ -1,6 +1,6 @@
-class ClaimCrate: ExileAbstractAction
-{
-    title = "Claim Ownership";
-    condition = "true";
-    action = "call ExileClient_ClaimCrates_network_claimRequestSend";
-};
+            class ClaimCrate: ExileAbstractAction
+            {
+                title = "Claim Ownership";
+                condition = "true";
+                action = "call ExileClient_ClaimCrates_network_claimRequestSend";
+            };
